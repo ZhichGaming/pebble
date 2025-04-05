aa511f9149a6e3274525bbc75cc323bf95738712
Nvadidivan <navid18lang@gmail.com>
2025-04-05 12:17:00 -0400

    what could possibly go wrong

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,15 +1,21 @@
+import Link from 'next/link';
 import React from 'react';
+import "@/components/globals.css";
+
 
 export default function NavigationBar() {
 	return (
-		<nav className="w-screen">
+		<nav className="fixed w-screen bg-background">
 			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 				<div className="flex items-center justify-between h-16">
 					<div className="flex items-center">
 						<img src="../favicon.ico" alt="" className='h-6 mr-4' />
 						<div className="text-xl font-bold text-[#595F39]">Pebble</div>
 					</div>
-					
+				<div>
+					<Link className="border-2 border-[#595F39] rounded-lg bg-[#595F39] text-[#E4E4DE] py-1.5 px-3 mx-2" href="./login">Login</Link>
+					<Link className="border-2 border-[#595F39] rounded-lg py-1.5 px-3 mx-2" href="./signup">Sign up</Link>
+				</div>
 				</div>
 			</div>
 		</nav>
