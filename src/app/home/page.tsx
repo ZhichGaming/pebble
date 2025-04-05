ec32f9e0742401a815796032456ba1606a126de0
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2025-04-05 14:02:57 -0400

    fixed home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { ClientUser, Subject, User } from "@/lib/mongodb/schema";
+import { ClientUser, Subject } from "@/lib/mongodb/schema";
 import React from "react";
-import Image from 'next/image'
+import Image from "next/image";
 import userImagePath from "@/assets/user.png";
 import cubeImagePath from "@/assets/3d-cube.png";
 import Link from "next/link";
@@ -21,7 +21,7 @@ export default function HomePage() {
         return;
       }
 
-      setUser(data);
+      setUser(JSON.parse(data));
     };
 
     const fetchSubjects = async () => {
@@ -31,7 +31,7 @@ export default function HomePage() {
         return;
       }
 
-      setSubjects(data as Subject[]);
+      setSubjects(JSON.parse(data) as Subject[]);
     };
 
     fetchUser();
@@ -39,55 +39,78 @@ export default function HomePage() {
   }, []);
 
   return (
-		<div className="flex flex-col items-center justify-center w-screen h-screen">
-			<div className="flex flex-grow w-screen px-8 overflow-y-scroll mt-16">
-				{/* Left Column */}
-				<div className="flex-grow">
-					<h2 className="text-xl font-bold mb-4">Subjects</h2>
-					{subjects.map((subject) => (
-						<div key={subject._id.toString()} className="bg-white rounded-lg shadow p-5 mb-4 text-[#1B1B1B]">
-							<h1 className="font-bold text-xl mb-2">{subject.name}</h1>
-							{/* <hr className="my-2" /> */}
-							<div className="flex gap-x-4 opacity-60">
-								<div className="flex mb-1 items-center">
-									<Image src={userImagePath} alt="User" className="w-4 h-4 mr-2" />
-									<p>{subject.teachers.length} teachers</p>
-								</div>
-								<div className="flex mb-1 items-center">
-									<Image src={cubeImagePath} alt="Concepts" className="w-4 h-4 mr-2" />
-									<p>{subject.teachers.map(t => t.concepts.length).reduce((acc, curr) => curr + acc)} concepts</p>
-								</div>
-							</div>
-							<hr className="my-2" />
-							<div className="flex mb-1 items-center">
-								<p>Select a Teacher</p>
-							</div>
-							<div className="flex">
-								{subject.teachers.map((teacher) => (
-									<Link href={`${subject._id.toString()}/${encodeURIComponent(teacher.name)}`} key={teacher.name} className="mt-2 text-sm border rounded-full px-2 hover:bg-gray-100 mr-2">
-										<p>{teacher.name}</p>
-									</Link>
-								))}
-							</div>
-						</div>
-					))}
-				</div>
+    <div className="flex flex-col items-center justify-center w-screen h-screen">
+      <div className="flex flex-grow w-screen px-8 overflow-y-scroll mt-16">
+        {/* Left Column */}
+        <div className="flex-grow">
+          <h2 className="text-xl font-bold mb-4">Subjects</h2>
+          {subjects.map((subject) => (
+            <div
+              key={subject._id.toString()}
+              className="bg-white rounded-lg shadow p-5 mb-4 text-[#1B1B1B]"
+            >
+              <h1 className="font-bold text-xl mb-2">{subject.name}</h1>
+              {/* <hr className="my-2" /> */}
+              <div className="flex gap-x-4 opacity-60">
+                <div className="flex mb-1 items-center">
+                  <Image
+                    src={userImagePath}
+                    alt="User"
+                    className="w-4 h-4 mr-2"
+                  />
+                  <p>{subject.teachers.length} teachers</p>
+                </div>
+                <div className="flex mb-1 items-center">
+                  <Image
+                    src={cubeImagePath}
+                    alt="Concepts"
+                    className="w-4 h-4 mr-2"
+                  />
+                  <p>
+                    {subject.teachers
+                      .map((t) => t.concepts.length)
+                      .reduce((acc, curr) => curr + acc)}{" "}
+                    concepts
+                  </p>
+                </div>
+              </div>
+              <hr className="my-2" />
+              <div className="flex mb-1 items-center">
+                <p>Select a Teacher</p>
+              </div>
+              <div className="flex">
+                {subject.teachers.map((teacher) => (
+                  <Link
+                    href={`${subject._id.toString()}/${encodeURIComponent(
+                      teacher.name
+                    )}`}
+                    key={teacher.name}
+                    className="mt-2 text-sm border rounded-full px-2 hover:bg-gray-100 mr-2"
+                  >
+                    <p>{teacher.name}</p>
+                  </Link>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
 
-				{/* Vertical Separator */}
-				<div className="sticky top-3 w-px bg-gray-300 mx-8 my-3"></div>
+        {/* Vertical Separator */}
+        <div className="sticky top-3 w-px bg-gray-300 mx-8 my-3"></div>
 
-				{/* Right Column */}
-				<div className="sticky top-0 flex-1 w-64">
-					<h2 className="text-xl font-bold mb-4">Your Uploads</h2>
-					<div className="grid grid-cols-2 gap-4">
-						{/* {user.uploads.map((upload) => (
+        {/* Right Column */}
+        <div className="sticky top-0 flex-1 w-64">
+          <h2 className="text-xl font-bold mb-4">Your Uploads</h2>
+          <div className="grid grid-cols-2 gap-4">
+            {/* {user.uploads.map((upload) => (
 							<div key={upload.toString()} className="bg-white rounded-lg shadow p-5 mb-4 text-[#1B1B1B]">
 								<p>{upload.toString()}</p>
 							</div>
 						))} */}
-					</div>
-				</div>
-			</div>
-		</div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
-};
+}
+
