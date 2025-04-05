40d20257b5505332f78bdd48db29e3491294cf5c
ZhichGaming <nick.zhicheng@gmail.com>
2025-04-05 13:23:22 -0400

    fix: use client

--- src/app/[subject]/[teacher]/page.tsx
+++ src/app/[subject]/[teacher]/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useEffect, useState } from 'react';
 import { Concept, Subject, Teacher } from '@/lib/mongodb/schema';
 import { getSubjects } from '@/lib/subjects/actions';
