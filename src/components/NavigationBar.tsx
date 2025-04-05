636a8393fa67542766cf35db15ad8c20ca5ebbc1
ZhichGaming <nick.zhicheng@gmail.com>
2025-04-05 14:04:48 -0400

    fix: error due to undefined identity on navbar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -28,15 +28,15 @@ export default function NavigationBar() {
 						<img src="../favicon.ico" alt="" className='h-6 mr-4' />
 						<div className="text-xl font-bold text-[#595F39]">Pebble</div>
 					</div>
-					{!user ? <div >
+					{user?.identity ? <div>
+						<p>Hey, {user.identity?.username}</p>
+						<button className="border-2 border-[#595F39] rounded-lg bg-[#595F39] text-[#E4E4DE] py-1.5 px-3 mx-2" onClick={logout}>Logout</button>
+					</div>:
+					<div >
 						<Link className="border-2 border-[#595F39] rounded-lg bg-[#595F39] text-[#E4E4DE] py-1.5 px-3 mx-2" href="./login">Login</Link>
 						<Link className="border-2 border-[#595F39] rounded-lg py-1.5 px-3 mx-2" href="./signup">Sign up</Link>
-					</div>:
-					<div>
-						<p>Hey, {user.identity.username}</p>
-						<button className="border-2 border-[#595F39] rounded-lg bg-[#595F39] text-[#E4E4DE] py-1.5 px-3 mx-2" onClick={logout}>Logout</button>
-
-					</div>}
+					</div>
+					}
 					
 				</div>
 			</div>
