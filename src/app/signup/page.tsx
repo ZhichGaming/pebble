baeeb5824e555d6f00eda9f19ada86a3bf46759c
ZhichGaming <nick.zhicheng@gmail.com>
2025-04-05 02:07:02 -0400

    feat: login page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,18 +1,23 @@
-"use client";
+import { signup } from '@/lib/login/login';
+import React from 'react';
 
-import { useEffect } from "react";
-import { getSubjects, login, signup } from "@/lib/login/login";
-
-export default function LoginPage() {
-  useEffect(() => {
-    // getSubjects();
-  });
-
+export default function SignupPage() {
 	return (
 		<div className="flex items-center justify-center min-h-screen bg-[#E4E4DE]">
 			<div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
-				<h2 className="text-2xl font-bold text-center text-gray-800">Sign In</h2>
+				<h2 className="text-2xl font-bold text-center text-gray-800">Sign Up</h2>
 				<form className="space-y-4">
+					<div>
+						<label htmlFor="name" className="block text-sm font-medium text-gray-700">
+							Name
+						</label>
+						<input
+							type="text"
+							id="name"
+							className="w-full px-4 py-2 mt-1 border rounded-md"
+							placeholder="Enter your name"
+						/>
+					</div>
 					<div>
 						<label htmlFor="email" className="block text-sm font-medium text-gray-700">
 							Email
@@ -22,7 +27,6 @@ export default function LoginPage() {
 							id="email"
 							className="w-full px-4 py-2 mt-1 border rounded-md"
 							placeholder="Enter your email"
-              required
 						/>
 					</div>
 					<div>
@@ -34,26 +38,23 @@ export default function LoginPage() {
 							id="password"
 							className="w-full px-4 py-2 mt-1 border rounded-md"
 							placeholder="Enter your password"
-              required
 						/>
 					</div>
 					<button
 						type="submit"
-						className="w-full px-4 py-2 text-white bg-[#595F39] rounded-md hover:bg[#595F39] cursor-pointer"
-            // formAction={login} // TODO: Fix this
+						className="w-full px-4 py-2 text-white bg-[#595F39] rounded-md cursor-pointer"
+						formAction={signup}
 					>
-						Sign In
+						Sign Up
 					</button>
 				</form>
 				<p className="text-sm text-center text-gray-600">
-					Don't have an account?{' '}
-					<a href="/signup" className="text-[#595F39] hover:underline">
-						Sign up
+					Already have an account?{' '}
+					<a href="/login" className="text-[#595F39] hover:underline">
+						Log in
 					</a>
 				</p>
 			</div>
 		</div>
 	);
 };
-
-
